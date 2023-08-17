c5b745e627e55c5746458a01e6c1c8a679885d27
mircheus <mircheuss@yandex.ru>
2023-08-17 15:55:43 +0200

    add witch and WitchMovement.cs scripts

---
+++ Assets/Game/Scripts/Reader/Games/Witch/WitchMovement.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Reader.Games.ShootingGame
+{
+    public class WitchMovement : MonoBehaviour
+    {
+
+    }
+}
+
