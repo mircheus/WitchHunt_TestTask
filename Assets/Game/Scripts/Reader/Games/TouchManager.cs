b638df5c2013e62181f5ad31c1fdf9e8310e5b22
mircheus <mircheuss@yandex.ru>
2023-08-17 13:50:55 +0200

    delete TouchAction.inputactions, add Touch methods for debug

---
+++ Assets/Game/Scripts/Reader/Games/TouchManager.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TouchManager : MonoBehaviour
+{
+    [SerializeField] private InputAction screenPosition;
+    [SerializeField] private InputAction press;
+
+    private Camera _mainCamera;
+
+    private void Awake()
+    {
+        _mainCamera = Camera.main;
+    }
+
+    private void OnEnable()
+    {
+        screenPosition.Enable();
+        press.Enable();
+        press.performed += TouchPressed;
+    }
+    
+    private void OnDisable()
+    {
+        screenPosition.Disable();
+        press.Disable();
+        press.performed -= TouchPressed;
+    }
+    
+    private void TouchPressed(InputAction.CallbackContext context)
+    {
+        Vector3 touchPosition = screenPosition.ReadValue<Vector2>();
+        RaycastHit2D hit = Physics2D.Raycast(_mainCamera.ScreenToWorldPoint(touchPosition), Vector2.zero, Mathf.Infinity);
+        
+        if (hit.collider != null)
+        {
+            Debug.Log(hit.collider.gameObject.name);
+        }
+    }
+}
