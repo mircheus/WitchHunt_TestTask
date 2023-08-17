b3933529822b4bec477366130dae45d95623de48
mircheus <mircheuss@yandex.ru>
2023-08-17 16:08:06 +0200

    add basic movement

--- Assets/Game/Scripts/Reader/Games/Witch/WitchMovement.cs
+++ Assets/Game/Scripts/Reader/Games/Witch/WitchMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,41 @@ namespace Game.Reader.Games.ShootingGame
 {
     public class WitchMovement : MonoBehaviour
     {
+        [SerializeField] private Transform point1;
+        [SerializeField] private Transform point2;
+        [SerializeField] private float speed;
+        
+        private Vector2 _target;
 
+        private void Start()
+        {
+            _target = point1.position;
+        }
+
+        private void Update()
+        {
+            Move();
+        }
+
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Move()
+        {
+            transform.position = Vector2.MoveTowards(transform.position, _target, speed * Time.deltaTime);
+            
+            if (Vector2.Distance(transform.position, _target) < 0.1f)
+            {
+                ChangeTarget();
+            }
+        }
+
+        private void ChangeTarget()
+        {
+            _target = point2.position;
+        }
     }
 }
 
