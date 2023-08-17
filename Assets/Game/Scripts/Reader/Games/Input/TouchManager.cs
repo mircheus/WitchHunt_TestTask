b6046d4315809ec784c19d5bf336bf8375c8af66
mircheus <mircheuss@yandex.ru>
2023-08-17 14:44:33 +0200

    relocated TouchManager.cs file to Input folder

---
+++ Assets/Game/Scripts/Reader/Games/Input/TouchManager.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TouchManager : MonoBehaviour
+{
+    [SerializeField] private InputAction screenPosition;
+    [SerializeField] private InputAction press;
+    [SerializeField] private ShootSystem shootSystem;
+
+    private Camera _mainCamera;
+
+    private void Awake()
+    {
+        _mainCamera = Camera.main;
+    }
+
+    private void OnEnable()
+    {
+        screenPosition.Enable();
+        press.Enable();
+        press.performed += TouchPressed;
+    }
+    
+    private void OnDisable()
+    {
+        screenPosition.Disable();
+        press.Disable();
+        press.performed -= TouchPressed;
+    }
+    
+    private void TouchPressed(InputAction.CallbackContext context)
+    {
+        Vector2 touchPosition = screenPosition.ReadValue<Vector2>();
+        Vector2 position = _mainCamera.ScreenToWorldPoint(touchPosition);
+        
+        shootSystem.Shoot(position);
+    }
+}
