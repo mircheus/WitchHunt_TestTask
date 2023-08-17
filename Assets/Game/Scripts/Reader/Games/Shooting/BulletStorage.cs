eafe46b32dc4e7306356bbe276f7f06ed6d2d6b6
mircheus <mircheuss@yandex.ru>
2023-08-17 15:08:50 +0200

    add BulletStorage.cs methods for Initialize and GetBullet

--- Assets/Game/Scripts/Reader/Games/Shooting/BulletStorage.cs
+++ Assets/Game/Scripts/Reader/Games/Shooting/BulletStorage.cs
@@ -1,8 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletStorage : MonoBehaviour
+namespace Game.Reader.Games.Shooting
 {
+    public class BulletStorage : MonoBehaviour
+    {
+        [SerializeField] private Bullet[] bullets;
+        [SerializeField] private Bullet bulletPrefab;
+        [SerializeField] private int bulletCount;
+        [SerializeField] private GameObject mainParent;
 
+        private void Start()
+        {
+            InitializeStorage();
+        }
+
+        public Bullet GetBullet()
+        {
+            foreach (Bullet bullet in bullets)
+            {
+                if (bullet.gameObject.activeSelf == false)
+                {
+                    return bullet;
+                }
+            }
+
+            return null;
+        }
+
+        private void InitializeStorage()
+        {
+            bullets = new Bullet[bulletCount];
+            
+            for(int i = 0; i < bulletCount; i++)
+            {
+                bullets[i] = Instantiate(bulletPrefab, transform);
+                Debug.Log("Bullet created");
+            }
+        }
+    }
 }
+
--- Assets/Game/Scripts/Reader/Games/Shooting/ShootSystem.cs
+++ Assets/Game/Scripts/Reader/Games/Shooting/ShootSystem.cs
@@ -7,15 +7,16 @@ namespace Game.Reader.Games.Shooting
     public class ShootSystem : MonoBehaviour
     {
         [SerializeField] private Transform shootPoint;
-        [SerializeField] private Bullet bulletPrefab;
+        // [SerializeField] private Bullet bulletPrefab;
         [SerializeField] private GameObject mainParent;
+        [SerializeField] private BulletStorage bulletStorage;
     
         public void Shoot(Vector2 touchPosition)
         {
             Vector2 position = shootPoint.position;
             Vector2 direction = (touchPosition - position).normalized;
-            Bullet bullet = Instantiate(bulletPrefab, position, Quaternion.identity, mainParent.transform);
-            bullet.SetDirection(direction);
+            // Bullet bullet = Instantiate(bulletPrefab, position, Quaternion.identity, mainParent.transform);
+            // bullet.SetDirection(direction);
         }
     }
 }
