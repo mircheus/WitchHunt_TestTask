83178c45b33c5417e00ff73a0d6a22179a8c329c
mircheus <mircheuss@yandex.ru>
2023-08-18 13:44:48 +0200

    comment unnecessary fields

--- Assets/Game/Scripts/Reader/Games/Shooting/ShootSystem.cs
+++ Assets/Game/Scripts/Reader/Games/Shooting/ShootSystem.cs
@@ -9,9 +9,9 @@ namespace Game.Reader.Games.ShootingGame
     public class ShootSystem : MonoBehaviour
     {
         [SerializeField] private Transform shootPoint;
-        [SerializeField] private GameObject mainParent;
+        // [SerializeField] private GameObject mainParent;
         [SerializeField] private BulletPool bulletPool;
-        [SerializeField] private Bullet bulletPrefab;
+        // [SerializeField] private Bullet bulletPrefab;
 
         private SlingshotAnimation _slingshotAnimation;
         private bool _isAbleToShoot = true;
