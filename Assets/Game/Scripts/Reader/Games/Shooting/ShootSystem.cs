f2473bf6eeb93c518f7e204db755862a45f83a77
mircheus <mircheuss@yandex.ru>
2023-08-18 12:46:41 +0200

    add basic shooting animation

--- Assets/Game/Scripts/Reader/Games/Shooting/ShootSystem.cs
+++ Assets/Game/Scripts/Reader/Games/Shooting/ShootSystem.cs
@@ -1,16 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Game.Reader.Games.ShootingGame
 {
+    [RequireComponent(typeof(SlingshotAnimation))]
     public class ShootSystem : MonoBehaviour
     {
         [SerializeField] private Transform shootPoint;
         [SerializeField] private GameObject mainParent;
         [SerializeField] private BulletPool bulletPool;
         [SerializeField] private Bullet bulletPrefab;
-    
+
+        private SlingshotAnimation _slingshotAnimation;
+        
+        private void Awake()
+        {
+            _slingshotAnimation = GetComponent<SlingshotAnimation>();
+        }
+
         public void Shoot(Vector2 touchPosition)
         {
             Vector2 position = shootPoint.position;
@@ -21,6 +30,7 @@ namespace Game.Reader.Games.ShootingGame
             bullet.transform.position = shootPoint.position;
             bullet.SetDirection(direction);
             bullet.gameObject.SetActive(true);
+            _slingshotAnimation.PlayShootAnimation();
         }
     }
 }
---
+++ Assets/Game/Scripts/Reader/Games/Shooting/SlingshotAnimation.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlingshotAnimation : MonoBehaviour
+{
+    private Animator _animator;
+    private int _prepareAnimation = Animator.StringToHash("Prepare"); 
+    private int _shootAnimation = Animator.StringToHash("Shoot");
+    
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void PlayShootAnimation()
+    {
+        _animator.Play(_shootAnimation);
+    }
+}
